771346400cb798bf7159cb0601faac2ad8ee7d2c
Jake Klassen <jklassendev@gmail.com>
2023-12-19 16:07:09 -0500

    update starter with resizing and fullscreen

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
-import "./style.css";
 import * as THREE from "three";
+import { OrbitControls } from "three/addons/controls/OrbitControls.js";
+import "./style.css";
 
 const canvas = document.querySelector<HTMLCanvasElement>("canvas.webgl");
 
@@ -7,25 +8,45 @@ if (canvas == null) {
 	throw new Error("Canvas not found");
 }
 
+const sizes = {
+	width: window.innerWidth,
+	height: window.innerHeight,
+};
+
+window.addEventListener("resize", () => {
+	sizes.width = window.innerWidth;
+	sizes.height = window.innerHeight;
+
+	camera.aspect = sizes.width / sizes.height;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize(sizes.width, sizes.height);
+	renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+});
+
+window.addEventListener("dblclick", () => {
+	if (document.fullscreenElement) {
+		document.exitFullscreen();
+	} else {
+		canvas.requestFullscreen();
+	}
+});
+
 const scene = new THREE.Scene();
 
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 
 const mesh = new THREE.Mesh(geometry, material);
-mesh.rotateOnAxis(new THREE.Vector3(1, 1, 1).normalize(), 0.5);
 scene.add(mesh);
-
-const sizes = {
-	width: 800,
-	height: 600,
-};
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 
-camera.position.z = 3;
-camera.lookAt(mesh.position);
+camera.position.set(0, 0, 3);
 scene.add(camera);
+
+const controls = new OrbitControls(camera, canvas);
+controls.enableDamping = true;
 
 const renderer = new THREE.WebGLRenderer({
 	antialias: true,
@@ -33,5 +54,14 @@ const renderer = new THREE.WebGLRenderer({
 });
 
 renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 
-renderer.render(scene, camera);
+function frame() {
+	controls.update();
+
+	renderer.render(scene, camera);
+
+	requestAnimationFrame(frame);
+}
+
+requestAnimationFrame(frame);
